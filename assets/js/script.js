137f15f39087a5939bdeb718553114d3cef75392
YeBa <yeletta.banks@gmail.com>
2023-04-30 22:39:08 -0700

    add weather branch functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,38 @@
-var requestUrl1='https://api.open-meteo.com/v1/forecast?latitude=37.87&longitude=-122.27&hourly=temperature_2m';
+ 
+ 
+ const todaysWeatherBtn = document.querySelector('#todaysWeather')
+ const result = document.querySelector('#result')
+ 
+ function getWeatherApi (){
+
+ var requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=37.87&longitude=-122.27&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&precipitation_unit=inch&timeformat=unixtime&forecast_days=1&timezone=America%2FLos_Angeles';
 
 
-fetch (requestUrl1)
-  .then((function(response) {
+  fetch(requestUrl) 
+  .then(function (response) {
     return response.json();
-  
   })
-  .then (function(data){
-    console.log(response)
+  .then(function (data) {
+    console.log(data);
 
-var todaysWeather = document.createElement('h1');
+  //get temps from array
+    var dayHigh =data.daily.temperature_2m_max['0'];
+    var dayLow =data.daily.temperature_2m_min['0'];
+    
+    let highTemp = dayHigh;
+    let lowTemp = dayLow;
+    
+    //checks
+    console.log(highTemp);
+    console.log(lowTemp);
+    
 
+    high.innerText ="Today's High Temperature: " + dayHigh
+    low.innerText ="Today's Low Temperature: " + dayLow
+    
+  });
+}
 
+todaysWeatherBtn.addEventListener("click",getWeatherApi)
 
-var requestUrl2='https://api.sampleapis.com/coffee/hot';
-
-
-fetch (requestUrl2)
-  .then((function(response) {
-    return response.json();
-  
-  })
-  .then (function(data){
-    for (var i=0; i < data.length; i++) {
-      var createHotcard = document.createElement('tr');
-      var tableHotdata = document.createElement('td');
-      var infoHot = document.createElement ('li');
-
-      infoHot.textContent = data[].description;
-
-      tableHotdata.appendChild(infoHot);
-      createCardBody.appendChild(tableHotdata);
-      tableHotbody.appendChild(createCardbody);
-
-    }
-  }))
-
-  fetchButton.addEventListener('click,getAPI');
-getApi(requestUrl);
+getWeatherApi();
