a68a869714d6b3f5a50d3491f15422a25dbdcdde
JonathanAbbema <abbema44@gmail.com>
2023-05-01 19:18:28 -0700

    incorporated the weather API call as well as updated the HTML to be accurate

--- script.js
+++ script.js
@@ -174,36 +174,36 @@ function getColdIng1() {
 
   }
 
-  function getColdIng6() {
+  //function getColdIng6() {
     // fetch request gets a list of the following iced Coffee Data: Description, ID, Image, Ingredients, and Title
-    var requestUrl = 'https://api.sampleapis.com/coffee/iced';
+   // var requestUrl = 'https://api.sampleapis.com/coffee/iced';
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
+    //fetch(requestUrl)
+      //.then(function (response) {
+        //return response.json();
+     // })
+      //.then(function (data) {
 
-        console.log(data)
-        for (var i = 5; i < 6; i++) {
+       // console.log(data)
+        //for (var i = 5; i < 6; i++) {
           // Creating elements, tablerow, tabledata, and anchor
-          var createTableRow = document.createElement('tr');
-          var tableData = document.createElement('td');
-          var ing = document.createElement('li');
+          //var createTableRow = document.createElement('tr');
+          //var tableData = document.createElement('td');
+          //var ing = document.createElement('li');
 
-          ing.textContent = data[5].ingredients
-          var apiData = ing.textContent
+          //ing.textContent = data[5].ingredients
+          //var apiData = ing.textContent
 
-          tableData.appendChild(ing);
-          createTableRow.appendChild(tableData);
-          coldCard6.appendChild(createTableRow);
+          //tableData.appendChild(ing);
+          //createTableRow.appendChild(tableData);
+          //coldCard6.appendChild(createTableRow);
 
           //console.log(apiData);
 
-        }
-      });
+       // }
+      //});
 
-  }
+  //}
 
 
 ing1.addEventListener('click', getColdIng1)
@@ -211,7 +211,7 @@ ing2.addEventListener('click', getColdIng2)
 ing3.addEventListener('click', getColdIng3)
 ing4.addEventListener('click', getColdIng4)
 ing5.addEventListener('click', getColdIng5)
-ing6.addEventListener('click', getColdIng6)
+//ing6.addEventListener('click', getColdIng6)
 
 
 
@@ -232,11 +232,50 @@ ing6.addEventListener('click', getColdIng6)
      if (coldCard5.style.display === 'none') {
       coldCard5.style.display = 'block';
     }
-     if (coldCard6.style.display === 'none') {
-      coldCard6.style.display = 'block';
-    }
+    // if (coldCard6.style.display === 'none') {
+     // coldCard6.style.display = 'block';
+   // }
     else {
       style.display = "none"
     }
   });
 
+
+//Weather API Call 
+
+  const todaysWeatherBtn = document.querySelector('#todaysWeather')
+  const result = document.querySelector('#result')
+  
+  function getWeatherApi (){
+ 
+  var requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=37.87&longitude=-122.27&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&precipitation_unit=inch&timeformat=unixtime&forecast_days=1&timezone=America%2FLos_Angeles';
+ 
+ 
+   fetch(requestUrl) 
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (data) {
+     console.log(data);
+ 
+   //get temps from array
+     var dayHigh =data.daily.temperature_2m_max['0'];
+     var dayLow =data.daily.temperature_2m_min['0'];
+     
+     let highTemp = dayHigh;
+     let lowTemp = dayLow;
+     
+     //checks
+     console.log(highTemp);
+     console.log(lowTemp);
+     
+ 
+     high.innerText ="Today's High Temperature: " + dayHigh
+     low.innerText ="Today's Low Temperature: " + dayLow
+     
+   });
+ }
+ 
+ todaysWeatherBtn.addEventListener("click",getWeatherApi)
+ 
+ getWeatherApi();
